1fe91c7865e77a8f237698959f8739f72a5423f8
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-05 10:28:39 +0545

    Project Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
 
 const App = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
   return (
-    <div className='bg-blue-400'>
-      Hi
+    <div>
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
