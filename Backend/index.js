2e258657d397cdbef2883607c0bd7cb31984af7f
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-06 13:05:23 +0545

    Backend Setup

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./config/database.js";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//cors
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOption));
+
+//routes
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Example app listening on port ${port}`);
+  });
