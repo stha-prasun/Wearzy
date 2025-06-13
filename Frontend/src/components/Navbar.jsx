97385fae6c8916c002fdf045d361d010f21585f8
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-13 13:16:18 +0545

    Admin Control

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { MdAdminPanelSettings } from "react-icons/md";
 
 function Navbar() {
+  const user = useSelector((store) => store.User.user);
+
   return (
     <div className="navbar bg-base-100 shadow-sm pr-[20px]">
       {/* Navbar Start: Hamburger Menu */}
@@ -64,10 +68,7 @@ function Navbar() {
 
       {/* Navbar Center: Logo */}
       <div className="navbar-center">
-        <NavLink
-          to="/"
-          className="text-2xl font-bold"
-        >
+        <NavLink to="/" className="text-2xl font-bold">
           Wearzy
         </NavLink>
       </div>
@@ -75,38 +76,58 @@ function Navbar() {
       {/* Navbar End: Cart and Profile */}
       <div className="navbar-end">
         {/* Cart */}
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
+        {user?.role == "user" && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">
+                  {user?.orders?.length || 0}
+                </span>
+              </div>
             </div>
-          </div>
-          <div
-            tabIndex={0}
-            className="mt-3 z-10 card card-compact dropdown-content w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
+            <div
+              tabIndex={0}
+              className="mt-3 z-10 card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="text-lg font-bold">
+                  {user?.orders?.length || 0} Items
+                </span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
+
+        {user?.role == "admin" && (
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <MdAdminPanelSettings size={28} />
+            </div>
+          </div>
+        )}
 
         {/* Profile Avatar */}
         <div className="dropdown dropdown-end ml-2">
@@ -115,14 +136,14 @@ function Navbar() {
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <FaRegUser size={20}/>
+            <FaRegUser size={20} />
           </div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <NavLink to="/profile">Profile</NavLink>
+              <NavLink to="/profile">{user?.fullname}</NavLink>
             </li>
             <li>
               <NavLink to="/settings">Settings</NavLink>
