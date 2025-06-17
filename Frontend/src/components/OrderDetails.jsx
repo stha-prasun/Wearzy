b44ce6b442366d675385e3e7bcfd53cd744d3692
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-06-17 09:57:36 +0545

    Get Order Details

--- Frontend/src/components/OrderDetails.jsx
+++ Frontend/src/components/OrderDetails.jsx
@@ -1,6 +1,30 @@
+import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
+import { ORDER_API_ENDPOINT } from "../utils/constants";
+import axios from "axios";
 
 const OrderDetails = () => {
+  const { id } = useParams();
+  const [Order, setOrder] = useState();
+
+  useEffect(() => {
+    const handleCall = async () => {
+      try {
+        const response = await axios.get(`${ORDER_API_ENDPOINT}/get/${id}`, {
+          withCredentials: true,
+        });
+
+        if (response.data.success) {
+          console.log(response.data.order);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleCall();
+  }, [id]);
+
   const order = {
     _id: "ORD123456",
     createdAt: "2025-06-11T03:41:10.319Z",
@@ -31,7 +55,9 @@ const OrderDetails = () => {
     <>
       <Navbar />
       <div className="max-w-5xl mx-auto p-6">
-        <h1 className="text-3xl font-bold text-indigo-700 mb-6">Order Summary</h1>
+        <h1 className="text-3xl font-bold text-indigo-700 mb-6">
+          Order Summary
+        </h1>
 
         {/* Basic Info */}
         <div className="bg-white rounded-xl shadow p-6 mb-6 space-y-4">
@@ -46,11 +72,7 @@ const OrderDetails = () => {
             </div>
             <div>
               <h2 className="font-semibold text-md">Status</h2>
-              <span
-                className="badge badge-info"
-              >
-                {order.orderStatus}
-              </span>
+              <span className="badge badge-info">{order.orderStatus}</span>
             </div>
           </div>
 
@@ -69,7 +91,9 @@ const OrderDetails = () => {
 
         {/* Items */}
         <div className="bg-white rounded-xl shadow p-6 mb-6">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">Ordered Items</h2>
+          <h2 className="text-xl font-semibold text-gray-800 mb-4">
+            Ordered Items
+          </h2>
           <div className="space-y-4">
             {order.orderItems.map((item, idx) => (
               <div
